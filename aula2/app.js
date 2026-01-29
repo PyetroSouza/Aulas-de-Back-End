24c6c4e0a393bce90782249e3d20ad485a796bc7
PYETRO FERREIRA DE SOUZA <25205137@cfp127aluno.local>
2026-01-29 13:43:02 -0300

    files: adicionando pastas das aulas

---
+++ aula2/app.js
@@ -0,0 +1,22 @@
+/**************************************************** 
+ * Objetivo: Projeto para realizar o cálculo de médias escolares
+ * Autor: Pyetro Souza
+ * Data: 29/01/2026
+ * Versão: 1.0
+ * **************************************************/
+
+/* 
+    Tipos de criação de variáveis
+
+    var -> Permite criar um espaço em memória do tipo variável.
+            Essa forma de criação hoje é considerada mias antiga, não sendo utilizada em projetos mais novos, 
+            é provável que seja encontrada apenas em projetos mais antigos.
+            Dica: caso você precise utilizar o var, recomenda-se 
+            que seja utiliado apenas em escopo global (criado começo do projeto, e precisa para o todo projetos, em funções principalmente)
+            Escobo local: apenas dentro do bloco.
+
+    let -> 
+
+    const: 
+
+*/
