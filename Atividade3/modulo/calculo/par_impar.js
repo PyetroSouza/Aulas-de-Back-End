fcfbba42aefa92e6946f62501d8eb9bd2b2fb9b7
Pyetro Ferreira <pyetroferreirasouzza9@gmail.com>
2026-03-02 20:49:25 -0300

    feat: cálculo fatorial feito

--- Atividade3/modulo/calculo/par_impar.js
+++ Atividade3/modulo/calculo/par_impar.js
@@ -1,3 +1,10 @@
+/**********************************************************
+ * Objetivo: Arquivo responsável pelo cálculo de par ou ímpars
+ * Data: 28/02/2026
+ * Autor: Pyetro Ferreira
+ * Versão: 1.0
+ ***********************************************************/
+
 const calcularParOuImpar = function(numero1, numero2){
     numInicial = Number(numero1)
     numFInal = Number(numero2)
