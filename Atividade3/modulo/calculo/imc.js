d4869c23971a3dbb5044c2e43ee0420bded9abb1
Pyetro Ferreira <pyetroferreirasouzza9@gmail.com>
2026-03-03 19:04:06 -0300

    feat: adicionar função validar unidade de medida da altura do IMC

--- Atividade3/modulo/calculo/imc.js
+++ Atividade3/modulo/calculo/imc.js
@@ -4,37 +4,60 @@
  * Autor: Pyetro Ferreira
  * Versão: 1.0
  ***********************************************************/
-//Função para calcular IMC, recebendo peso e altura
 
+
+const validarUnidadeMedidaAltura = function (unidadeMedida) {
+    let medida = unidadeMedida.toUpperCase()
+    if (medida === "CM" || medida === "M") {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+//Função para calcular IMC, recebendo peso e altura
 const calcularImc = function (peso, altura) {
     let pesoPessoa = Number(peso)
     let alturaPessoa = Number(altura)
 
-    return pesoPessoa / (alturaPessoa * alturaPessoa)
+        if (medida === 'CM') {
+        alturaUsuario = alturaUsuario / 100
+    } else if (medida !== "M") {
+        return false
+    }
+
+   imc = (pesoPessoa / (alturaPessoa) ** 2).toFixed(2)
+   return imc
 }
 
 const classificarImc = function (imc) {
+    let imcClassic = imc
+    let classificarImc = ""
+    if (imcClassic < 18.5)
+        classificarImc = 'Abaixo do peso'
 
-    if (imc < 18.5)
-        return 'Abaixo do peso'
+    else if (imcClassic >= 18.5 && imcClassic <= 24.9)
+        classificarImc = 'Peso normal'
 
-    else if (imc >= 18.5 && imc <= 24.9)
-        return 'Peso normal'
+    else if (imcClassic >= 25 && imcClassic <= 29.9)
+        classificarImc = 'Sobrepeso'
 
-    else if (imc >= 25 && imc <= 29.9)
-        return 'Sobrepeso'
+    else if (imcClassic >= 30 && imcClassic <= 34.9)
+        classificarImc =  'Obesidade I'
 
-    else if (imc >= 30 && imc <= 34.9)
-        return 'Obesidade I'
+    else if (imcClassic >= 35 && imcClassic <= 39.9)
+        classificarImc = 'Obesidade II'
 
-    else if (imc >= 35 && imc <= 39.9)
-        return 'Obesidade II'
-
-    else
-        return 'Obesidade III'
+    else if (imcClassic > 40)
+        classificarImc = 'Obesidade III'
+    else {
+        return false
+    } return classificarImc
 }
 
 module.exports = {
     calcularImc,
-    classificarImc
+    classificarImc,
+    validarUnidadeMedidaAltura
 }
